f6a33196cb17b55724ad224ddac8f760827fe618
Evgeny <ku7nec0v@gmail.com>
2019-11-25 21:33:05 +0300

    refactor: opportunity to change collection added

--- soccer_stats/soccer_stats/mongo.py
+++ soccer_stats/soccer_stats/mongo.py
@@ -1,20 +1,22 @@
 from pymongo import MongoClient
+from soccer_stats.settings import MONGO_URI, MONGO_DATABASE
 
 
 class StatsMongoClient:
 
-    def __init__(self, uri, database, collection):
+    def __init__(self, uri, database):
         self.__connection = MongoClient(uri)
         self.__database = self.__connection[database]
-        self.__collection = self.__database[collection]
 
     @property
     def collection(self):
-        return self.__collection
+        if self.__collection:
+            return self.__collection
+        return None
     
     @collection.setter
     def collection(self, collection: str):
-        self.__collection = collection
+        self.__collection = self.__database[collection]
 
     @property
     def db(self):
@@ -22,3 +24,6 @@ class StatsMongoClient:
 
     def close(self):
         self.__connection.close()
+
+
+mongo_client = StatsMongoClient(MONGO_URI, MONGO_DATABASE)
